acd0f4f7b4155f1656d986367d658cf91690fd69
Fabio Donizeti Sinobre <fabiosinobre@gmail.com>
2026-05-10 11:03:03 +0000

    Configuração do ambiente Virtual

---
+++ main.py
@@ -0,0 +1,8 @@
+import sys
+def testeAmbiente():
+    print("_" * 30)
+    print("Olá, aluno! O nosso ambiente inicial está pronto.")
+    print(f"Versão do python: {sys.version}" )
+
+if __name__ == "__main__":
+    testeAmbiente()
